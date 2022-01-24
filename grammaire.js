07fca79bbb0175b5df44d38a47e342d35f398f52
BlackWorld72 <58783535+BlackWorld72@users.noreply.github.com>
2022-01-24 14:25:16 +0100

    Add files via upload

--- grammaire.js
+++ grammaire.js
@@ -89,22 +89,253 @@ grammaire =
                 }
             ]
         },
-        {
-            "words": [
+        { 
+            "words" : [
                 {
-                    "fr": "Blabla",
-                    "al": "Niederlage"
+                    "fr":" traduire",
+                    "al":"übersetzen (aus + dat // in + akk) "
                 },
                 {
-                    "fr": "Blablou",
-                    "al": "Selbstmord begehen"
+                    "fr":" convaincre qql de qqlch",
+                    "al":"überzeugen (von + dat) "
                 },
                 {
-                    "fr": "Blibli",
-                    "al": "Der Untergang"
+                    "fr":" s'adapter / se régler",
+                    "al":"sich umstellen/sich einstellen (auf + akk) "
+                },
+                {
+                    "fr":" dialoguer avc qql",
+                    "al":"sich unterhalten (mit + dat // über + akk) "
+                },
+                {
+                    "fr":" se différencier",
+                    "al":"sich unterscheiden (von/in + dat // durch + akk) "
+                },
+                {
+                    "fr":" cacher",
+                    "al":"verbergen/verheimlichen (vor + dat) "
+                },
+                {
+                    "fr":" relier qqch",
+                    "al":"verbinden (vor + dat) "
+                },
+                {
+                    "fr":" disposer de",
+                    "al":"verfügen (über + akk) "
+                },
+                {
+                    "fr":" poursuivre qql (en justice)",
+                    "al":"verklagen (auf + akk) "
+                },
+                {
+                    "fr":" compter sur qql",
+                    "al":"sich verlassen (auf + akk) "
+                },
+                {
+                    "fr":" inciter",
+                    "al":"verleiten (zu + dat) "
+                },
+                {
+                    "fr":" tomber amoureux",
+                    "al":"sich verlieben (in + akk) "
+                },
+                {
+                    "fr":" comprendre",
+                    "al":"verstehen (unter + dat) "
+                },
+                {
+                    "fr":" enfreindre",
+                    "al":"verstoßen (gegen + akk) "
+                },
+                {
+                    "fr":" se pencher sur qqlch",
+                    "al":"sich vertiefen (in + akk) "
+                },
+                {
+                    "fr":" avoir confiance",
+                    "al":"vertrauen (auf + akk) "
+                },
+                {
+                    "fr":" se préparer",
+                    "al":"sich vorbereiten (auf + akk) "
+                },
+                {
+                    "fr":" avertir qql ",
+                    "al":"warnen (vor + dat) "
+                },
+                {
+                    "fr":" attendre",
+                    "al":"warten (auf + akk) "
+                },
+                {
+                    "fr":" résister",
+                    "al":"sich wehren (gegen + akk) "
+                },
+                {
+                    "fr":" renoncer à",
+                    "al":"verzichten (auf + akk) "
+                }
+            ]
+        },
+        { 
+            "words" : [
+                {
+                    "fr":" faire de la publicité",
+                    "al":"werben (für + akk) "
+                },
+                {
+                    "fr":" savoir",
+                    "al":"wissen (von + dat // über + akk) "
+                },
+                {
+                    "fr":" s'étonner",
+                    "al":"ich wundern (über + akk) "
+                },
+                {
+                    "fr":" compter",
+                    "al":"zählen (zu + dat) "
+                },
+                {
+                    "fr":" ramener qql/qqlch",
+                    "al":"zurückführen (auf + akk) "
+                },
+                {
+                    "fr":" douter",
+                    "al":"zweifeln (an + dat) "
+                },
+                {
+                    "fr":" forcer ",
+                    "al":"zwingen (zu + dat) "
+                },
+                {
+                    "fr":" être dépendant",
+                    "al":"abhängig sein / angewiesen sein (von + dat // auf + akk) "
+                },
+                {
+                    "fr":" adresser",
+                    "al":"adressiert sein (an + akk) "
+                },
+                {
+                    "fr":" être reconnu",
+                    "al":"anerkannt sein (als + nom) "
+                },
+                {
+                    "fr":" être ouvert à qqlch",
+                    "al":"aufgeschlossen sein (gegenüber + dat) "
+                },
+                {
+                    "fr":" être impressionné",
+                    "al":"beeindruckt sein (von + dat) "
+                },
+                {
+                    "fr":" être ami",
+                    "al":"befreundet sein (mit + dat) "
+                },
+                {
+                    "fr":" être enthousiaste",
+                    "al":"begeistert sein (von + dat) "
+                },
+                {
+                    "fr":" aider qql",
+                    "al":"behilflich sein (bei + dat) "
+                },
+                {
+                    "fr":" être connu",
+                    "al":"bekannt sein (als + nom // bei + dat // für + akk) "
+                },
+                {
+                    "fr":" être bien vu",
+                    "al":"beliebt sein (bei + dat) "
+                },
+                {
+                    "fr":" être prêt ",
+                    "al":"bereit sein (zu + dat) "
+                },
+                {
+                    "fr":" être occupé",
+                    "al":"beschäftigt sein (mit + dat) "
+                }
+            ]
+        },
+        { 
+            "words" : [
+                {
+                    "fr":" être inquiet",
+                    "al":"besorgt sein / beunruhigt sein (um // über + akk) "
+                },
+                {
+                    "fr":" être impliqué ",
+                    "al":"beteiligt sein (an + dat) "
+                },
+                {
+                    "fr":" être compétent",
+                    "al":"bewandert sein (auf/in + dat) "
+                },
+                {
+                    "fr":" être pale",
+                    "al":"blass sein (vor + dat) "
+                },
+                {
+                    "fr":" böse sein",
+                    "al":"böse sein (auf + akk) "
+                },
+                {
+                    "fr":" être caractéristique ",
+                    "al":"charakteristisch sein (für + akk) "
+                },
+                {
+                    "fr":" être reconnaissant ",
+                    "al":"dankbar sein (für + akk) "
+                },
+                {
+                    "fr":" être jaloux ",
+                    "al":"eifersüchtig sein (auf + akk) "
+                },
+                {
+                    "fr":" être d'accord ",
+                    "al":"einverstanden sein (mit + dat) "
+                },
+                {
+                    "fr":" être éloigné",
+                    "al":"entfernt sein (von + dat) "
+                },
+                {
+                    "fr":" être décisif ",
+                    "al":"entscheidend sein (für + akk) "
+                },
+                {
+                    "fr":" être horrifié ",
+                    "al":"entsetzt sein (über + akk) "
+                },
+                {
+                    "fr":" être déçu ",
+                    "al":"enttäuscht sein (von + dat) "
+                },
+                {
+                    "fr":" être expérimenté",
+                    "al":"erfahren sein (in + dat) "
+                },
+                {
+                    "fr":" se réjouir",
+                    "al":"erfreut sein (über + akk) "
+                },
+                {
+                    "fr":" être étonné ",
+                    "al":"erstaunt sein (über + akk) "
+                },
+                {
+                    "fr":" être capable de",
+                    "al":"fähig sein (zu + dat) "
+                },
+                {
+                    "fr":" avoir terminé ",
+                    "al":"fertig sein (mit + dat) "
+                },
+                {
+                    "fr":" être libre",
+                    "al":"frei sein (von + dat) "
                 }
             ]
         }
-        
    ]
 }
