670a77a835ebf2640a9d1029777a6d517715cfbe
BlackWorld72 <58783535+BlackWorld72@users.noreply.github.com>
2022-01-24 13:54:26 +0100

    Add files via upload

---
+++ grammaire.js
@@ -0,0 +1,110 @@
+grammaire =
+{
+    "cat": [
+        {
+            "words": [
+                {
+                    "fr": "gouter / avoir le gout",
+                    "al": "schmecken (nach + dat)"
+                },
+                {
+                    "fr": "écrire",
+                    "al": "schreiben (an +dat )"
+                },
+                {
+                    "fr": "protéger",
+                    "al": "(sich) schützen (vor + dat)"
+                },
+                {
+                    "fr": "voir",
+                    "al": "sehen (als + akk // inf mit.zu)"
+                },
+                {
+                    "fr": "se languir",
+                    "al": "sich sehnen (nach + dat)"
+                },
+                {
+                    "fr": "réduire",
+                    "al": "senken (um/auf + akk)"
+                },
+                {
+                    "fr": "baisser",
+                    "al": "ssinken (um/auf + akk)"
+                },
+                {
+                    "fr": "s'inquiéter",
+                    "al": "(sich) sorgen (für/um + akk)"
+                },
+                {
+                    "fr": "parler",
+                    "al": "sprechen (mit/von + dat // über +akk)"
+                },
+                {
+                    "fr": "provenir de",
+                    "al": "stammen (aus + dat)"
+                },
+                {
+                    "fr": "s'étonner",
+                    "al": "staunen (über + akk)"
+                },
+                {
+                    "fr": "augmenter",
+                    "al": "steigen (um/auf +akk)"
+                },
+                {
+                    "fr": "mourir",
+                    "al": "sterben (an + akk)"
+                },
+                {
+                    "fr": "voter",
+                    "al": "stimmen (für/gegen + akk)"
+                },
+                {
+                    "fr": "se disputer",
+                    "al": "sich streiten (mit + dat // über + akk)"
+                },
+                {
+                    "fr": "chercher",
+                    "al": "suchen (nach + dat)"
+                },
+                {
+                    "fr": "se tromper",
+                    "al": "sich täuschen (in + dat)"
+                },
+                {
+                    "fr": "participer à",
+                    "al": "teilnehmen (an + dat)"
+                },
+                {
+                    "fr": "téléphoner",
+                    "al": "telefonieren (mit + dat)"
+                },
+                {
+                    "fr": "rêver",
+                    "al": "träumen (von + dat)"
+                },
+                {
+                    "fr": "persuader qql de qqlch",
+                    "al": "überreden (zu + dat)"
+                }
+            ]
+        },
+        {
+            "words": [
+                {
+                    "fr": "Blabla",
+                    "al": "Niederlage"
+                },
+                {
+                    "fr": "Blablou",
+                    "al": "Selbstmord begehen"
+                },
+                {
+                    "fr": "Blibli",
+                    "al": "Der Untergang"
+                }
+            ]
+        }
+        
+   ]
+}
--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var cat
 var word
 var lang
+var sss
 
-function listWords() {
+function listWords(data) {
   qs = document.getElementById("qs")
   document.getElementById("qs").innerHTML = "";
+  sss = data
 
   select = document.createElement("select")
   select.setAttribute("onChange", "changeSelectAllWord(this.value)")
@@ -17,41 +19,33 @@ function listWords() {
   x = document.createElement("div")
   x.setAttribute("id", "jetest")
 
-  for (let j = 0 ; j < civilisation.cat.length ; j++) {
-    h2 = document.createElement("h2")
-    h2.textContent = "Catégorie : " + (j+1)
-    x.append(h2)
-
+  for (let j = 0 ; j < data.cat.length ; j++) {
     opt = document.createElement('option')
     opt.textContent = j+1
     opt.setAttribute("value", j+1);
     select.append(opt)
-
-    for (let i = 0 ; i < civilisation.cat[j].words.length ; i++) {
-      h2 = document.createElement("h3")
-      h2.setAttribute("id","inpDIV")
-      h2.textContent = civilisation.cat[j].words[i].fr + " > " + civilisation.cat[j].words[i].al
-      x.append(h2)
-    }
   }
 
   qs.append(select)
   qs.append(x)
+
+  changeSelectAllWord("all")
 }
 
 function changeSelectAllWord(value) {
+  console.log(sss)
   x = document.getElementById("jetest")
   x.innerHTML = "";
-
+  
   if (value == "all") {
-    for (let j = 0 ; j < civilisation.cat.length ; j++) {
+    for (let j = 0 ; j < sss.cat.length ; j++) {
       h2 = document.createElement("h2")
       h2.textContent = "Catégorie : " + (j+1)
       x.append(h2)
-      for (let i = 0 ; i < civilisation.cat[j].words.length ; i++) {
+      for (let i = 0 ; i < sss.cat[j].words.length ; i++) {
         h2 = document.createElement("h3")
         h2.setAttribute("id","inpDIV")
-        h2.textContent = civilisation.cat[j].words[i].fr + " > " + civilisation.cat[j].words[i].al
+        h2.textContent = sss.cat[j].words[i].fr + " > " + sss.cat[j].words[i].al
         x.append(h2)
       }
     }
@@ -61,29 +55,30 @@ function changeSelectAllWord(value) {
     h2 = document.createElement("h2")
     h2.textContent = "Catégorie : " + (j+1)
     x.append(h2)
-    for (let i = 0 ; i < civilisation.cat[j].words.length ; i++) {
+    console.log(j)
+    for (let i = 0 ; i < sss.cat[j].words.length ; i++) {
       h2 = document.createElement("h3")
       h2.setAttribute("id","inpDIV")
-      h2.textContent = civilisation.cat[j].words[i].fr + " > " + civilisation.cat[j].words[i].al
+      h2.textContent = sss.cat[j].words[i].fr + " > " + sss.cat[j].words[i].al
       x.append(h2)
     }
   }
-
+  console.log(sss)
 }
 
 function changeSelect() {
   x = document.getElementById("jetest")
   x.innerHTML = "";
-
+  data = sss
   value = document.getElementById("selecttest").value
   if (value == "all") {
-    cat = Math.floor(Math.random() * civilisation.cat.length)
+    cat = Math.floor(Math.random() * data.cat.length)
   }
   else {
     cat = parseInt(value)-1
   }
 
-  word = Math.floor(Math.random() * civilisation.cat[cat].words.length)
+  word = Math.floor(Math.random() * data.cat[cat].words.length)
   lang = Math.floor(Math.random() * 2)
 
   console.log(cat+ " "+ word + " ")
@@ -92,10 +87,10 @@ function changeSelect() {
   h2.setAttribute("id","inpDIV")
 
   if (lang == 1) {
-    h2.textContent = civilisation.cat[cat].words[word].fr
+    h2.textContent = data.cat[cat].words[word].fr
   }
   else {
-    h2.textContent = civilisation.cat[cat].words[word].al
+    h2.textContent = data.cat[cat].words[word].al
   }
 
   x.append(h2)
@@ -124,12 +119,14 @@ function validate() {
   document.getElementById("btnNext").setAttribute("style", "visibility: show;")
 }
 
-function question() {
+function question(data) {
   qs = document.getElementById("qs")
   document.getElementById("qs").innerHTML = "";
 
+  sss = data
+
   select = document.createElement("select")
-  select.setAttribute("onChange", "changeSelect(this.value)")
+  select.setAttribute("onChange", "changeSelect()")
   select.setAttribute("id", "selecttest")
 
   let opt = document.createElement('option')
@@ -140,7 +137,7 @@ function question() {
   x = document.createElement("div")
   x.setAttribute("id", "jetest")
 
-  for (let j = 0 ; j < civilisation.cat.length ; j++) {
+  for (let j = 0 ; j < data.cat.length ; j++) {
     opt = document.createElement('option')
     opt.textContent = j+1
     opt.setAttribute("value", j+1);
